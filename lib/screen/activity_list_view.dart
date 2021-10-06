01e65a5d027ae667fb9e3d7e56db38369b036dd1
Dimas maulana <54989440+Dimas-maulana@users.noreply.github.com>
2021-10-06 19:31:10 +0700

    Update activity_list_view.dart

--- lib/screen/activity_list_view.dart
+++ lib/screen/activity_list_view.dart
@@ -3,8 +3,6 @@ import 'package:personal_activity_app/provider/activity_notifier.dart';
 import 'package:provider/provider.dart';
 import 'input_activity_view.dart';
 
-//bagian ini bisa di ubah desainnya, warna atau diberi padding
-
 class ActivityScreen extends StatelessWidget {
 
   @override
@@ -25,7 +23,7 @@ class ActivityScreen extends StatelessWidget {
                   return InputActivity();
                 }));
               },
-                  child: Text("add new Activity")
+                  child: Text("Tambah aktifitas baru")
               ),
               Expanded(
                 child: Consumer<ActivityNotifier>( // harus dimasukan ke dalam widget consumer karena, untuk melakukan perubahan data yg akan ditampilkan
@@ -41,51 +39,56 @@ class ActivityScreen extends StatelessWidget {
                             // cara nya yaitu cnsm.activityList[index]
                           }));
                         },
-                        child: Card(//widget untuk menampilak item
-                          child: Padding(
-                          padding: EdgeInsets.all(8),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: <Widget>[
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                     Text(
-                                          'title: ${cnsm.activityList[index].name}',
-                                          // untuk mengambil nama diperlukan cara seperti diatas
-                                          style: TextStyle(fontSize: 18),
+                        child: Container(
+                          decoration: BoxDecoration(
+                                color: Colors.black,
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                          child: Card(//widget untuk menampilan item
+                            child: Padding(
+                            padding: EdgeInsets.all(8),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                       Text(
+                                            'Aktifitas : ${cnsm.activityList[index].name}',
+                                            // untuk mengambil nama diperlukan cara seperti diatas
+                                            style: TextStyle(fontSize: 17),
+                                          ),
+                                      Text(
+                                          'Deskripsi : ${cnsm.activityList[index].descActivity}',
+                                        // untuk mengambil deskripsi aktifitas diperlukan cara seperti diatas
+                                          style: TextStyle(fontSize: 17),
                                         ),
-                                    Text(
-                                        'todo: ${cnsm.activityList[index].descActivity}',
-                                      // untuk mengambil deskripsi aktifitas diperlukan cara seperti diatas
-                                        style: TextStyle(fontSize: 18),
-                                      ),
+                                       Text(
+                                          'Tanggal : ${cnsm.activityList[index].date}',
+                                         // untuk mengambil tangggal diperlukan cara seperti diatas
+                                          style: TextStyle(fontSize: 17),
+                                        ),
                                      Text(
-                                        'date: ${cnsm.activityList[index].date}',
-                                       // untuk mengambil tangggal diperlukan cara seperti diatas
-                                        style: TextStyle(fontSize: 18),
-                                      ),
-                                   Text(
-                                        'time: ${cnsm.activityList[index].time}',
-                                     // untuk mengambil waktu diperlukan cara seperti diatas
-                                        style: TextStyle(fontSize: 18),
-                                      ),
-                                  ],
-                                ),
-                                    OutlinedButton(
-                                      onPressed: (){
-                                      cnsm.deleteActivity(index); // sintaks ini digunakan untuk memanggil fungsi delet yg ada di data,
-                                      },
-                                      child: Text("Delete",
-                                        style: TextStyle(color: Colors.red
+                                          'Jam : ${cnsm.activityList[index].time}',
+                                       // untuk mengambil waktu diperlukan cara seperti diatas
+                                          style: TextStyle(fontSize: 17,),
+                                        ),
+                                    ],
+                                  ),
+                                      OutlinedButton(
+                                        onPressed: (){
+                                        cnsm.deleteActivity(index); // sintaks ini digunakan untuk memanggil fungsi delet yg ada di data,
+                                        },
+                                        child: Text("Delete",
+                                          style: TextStyle(color: Colors.red
+                                          ),
                                         ),
                                       ),
-                                    ),
-
-                              ],
+                                ],
+                              ),
                             ),
-                    ),
-                  ),
+                          ),
+                        ),
                       ),
                   itemCount: activityNotifier.activityList.length, // kita menggunakan activityNotifier karena data sudah menjadi bagian dari provider
                 ),
